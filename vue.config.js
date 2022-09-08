778b639af5add2c33798b8b68bc1fe3c53648336
Boldyreva Nadezhda <boldyrevanadezhda@gmail.com>
2022-09-08 23:30:53 +0700

    Добавлены Scss миксины

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // TODO:
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/scss/_mixins.scss";
+        `
+      }
+    }
+  }
 })
