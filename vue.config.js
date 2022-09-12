88f4e1cb80f95b1c7a09b9a8f9efac19318d2e93
Boldyreva Nadezhda <boldyrevanadezhda@gmail.com>
2022-09-12 11:23:34 +0700

    Fix config

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
     loaderOptions: {
       sass: {
         additionalData: `
+          @import "@/scss/_variables.scss";
           @import "@/scss/_mixins.scss";
         `
       }
